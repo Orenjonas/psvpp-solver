6f61de006c56bb792fb6fe9e727564b1f881ab33
Jonas Oren <jonas.oren@gmail.com>
2023-04-30 11:45:53 +0200

    Fixes and restructuring

--- psvpp_solver/constraints.py
+++ psvpp_solver/constraints.py
@@ -1,16 +1,17 @@
-# from threading import enumerate
-
 
 def check_constraints_satisfied(
-        genome: list[list[int]],
-        required_frequencies,
-        days_in_period=[1, 2, 3, 4, 5, 6, 7],
-        days_vessel_available: list[int] = [7, 5],
-        max_v_prepared: list[int] = [
-            2, 2, 2, 2, 2, 2, 2],
-        max_psv_prepared_per_day: int = 2,
-        departures_each_day: list[int] = [0, 0, 0, 0, 0, 0, 0]) -> bool:
-    """ Check that the given schedule passes the constraints:
+    genome: list[list[int]],
+    # TODO - change to len(period)
+    days_in_period=[1, 2, 3, 4, 5, 6, 7],
+    days_vessel_available: list[int] = [7, 5],
+    max_v_prepared: list[int] = [2, 2, 2, 2, 2, 1, 1],
+    max_psv_prepared_per_day: int = 2,
+    departures_each_day: list[int] = [0, 0, 0, 0, 0, 0, 0],
+        required_frequencies: list[int] = [2, 3, 2, 2]) -> bool:
+    """
+    Check that the given schedule passes the given constraints.
+
+     Constraints:
         - Sailling distance?
         - Duration
         - Number of installations
@@ -27,10 +28,10 @@ def check_constraints_satisfied(
             prepare for departure in day i, list of len n_days.
     """
     # (2) Ensure the required service frequency for each installation
-    for i, service_frequencies in enumerate(genome[1]):
-        n_visits = len(service_frequencies)
-        if n_visits < required_frequencies[i]:
-            print('Service frequency for installation', i,
+    for inst, services in enumerate(genome[1]):
+        n_visits = len(services)
+        if n_visits < required_frequencies[inst]:
+            print('Service frequency for installation', inst,
                   'not satisfied')
             print("Genome:", genome)
             return False
@@ -66,42 +67,69 @@ def check_constraints_satisfied(
     # (5) PSV cannot begin a voyage before returning from its previous one
     prev_depart = None
     for psv, departures in enumerate(genome[2]):
-        first_depart = departures[0]
-        last_depart = departures[-1]
-        if first_depart <= (last_depart + 1) % len(days_in_period):
-            print("Not enough time between journeys departing on day"
-                  "{first_depart} and {last_depart}, for psv {psv}"
-                  ".".format(first_depart=first_depart,
-                             last_depart=last_depart,
-                             psv=psv,)
-                  )
-            print("Genome:", genome)
-            return False
 
-        # TODO: Check boundary (check only one departure)
-
+        # First check difference between last and first departure in the period
+        prev_depart = departures[-1] - len(days_in_period)
         for departure_day in departures:
-            # Assumes all journeys are 1 day long and requires one day between
-            # returning and starting a new journey.
-            if prev_depart is not None:
-                # Check that there is one day between departures
-                if departure_day - prev_depart < 2:
-                    print("Not enough time between journeys departing on day "
-                          "{prev_depart} and {departure_day}, for psv {psv}"
-                          ".".format(prev_depart=prev_depart,
-                                     departure_day=departure_day,
-                                     psv=psv,)
-                          )
-                    print("Genome:", genome)
-                    return False
+            # Assumes all journeys are 1 day long and can sail the day after
+            # returning.
+
+            # Check that there is one day between departures
+            if departure_day - prev_depart < 1:
+                print("Not enough time between journeys departing on day "
+                      "{prev_depart} and {departure_day}, for psv {psv}"
+                      ".".format(prev_depart=prev_depart,
+                                 departure_day=departure_day,
+                                 psv=psv,)
+                      )
+                print("Genome:", genome)
+                return False
             prev_depart = departure_day
 
-        # (6) Make sure departures to each installation are properly spread
+    # (6) Make sure departures to each installation are properly spread
+    for inst, services in enumerate(genome[1]):
+        # Min and max distance between visits
+        f = required_frequencies[inst]
+        # df = len(days_in_period) / (f+1)
+        Pf_min = len(days_in_period) // (f+1)
+        Pf_max = Pf_min + 1
+        # Pf_min = f // df
+        # Pf_max = f // df + 1
 
-        # (7) Variable domains
+        # TODO: check [3, 4] and [1,3,4]
+
+        # What if only one visit? [1]
+
+        prev = services[-1] - len(days_in_period)
+        for day in services:
+            # Check diff within constraints
+            # TODO: Check definition of time between visits
+            days_between = day - prev - 1  # Number of days between services
+            if days_between < Pf_min or days_between > Pf_max:
+                # from IPython import embed
+                # embed()
+                print('day', day, 'prev', prev)
+                print('Services not sufficiently spread for  installation', inst)
+                print("Genome:", genome)
+                return False
+
+            prev = day
+
+    # (7) Variable domains
 
 
 if __name__ == "__main__":
+
+    #     genome = [
+    #         # tour [PSV][day] -> installations
+    #         [[[1, 2], [],     [4, 3, 2], []],
+    #          [[],     [3, 4], [],        [1, 2]]
+    #          ],
+    #         # installations [instsallation] -> day visited
+    #         [[1, 4], [1, 3, 4], [2, 3], [2, 3]],
+    #         # vessels [vessel] -> day departing
+    #         [[1, 3], [2, 4]]
+    #     ]
 
     genome = [
         # tour [PSV][day] -> installations
@@ -109,9 +137,9 @@ if __name__ == "__main__":
          [[],     [3, 4], [],        [1, 2]]
          ],
         # installations [instsallation] -> day visited
-        [[1, 4], [1, 3, 4], [2, 3], [2, 3]],
+        [[1, 4], [1, 3, 5], [2, 3], [2, 3]],
         # vessels [vessel] -> day departing
-        [[1, 3], [2, 4]]
+        [[1, 4], [2, 4]]
     ]
 
     # installations[installation][required service frequency]
@@ -121,3 +149,5 @@ if __name__ == "__main__":
         2,
         3
     ]
+
+    check_constraints_satisfied(genome)
