428946ae0814679bb5a782f1f1e3cc0765cc6a5f
Jonas Oren <jonas.oren@gmail.com>
2023-06-28 08:33:33 +0200

    Generate visits done

--- psvpp_solver/utils.py
+++ psvpp_solver/utils.py
@@ -1,10 +1,10 @@
 import numpy as np
+import random
 
 
 def generate_visits(n_installations: int,
                     n_days: int,
-                    days: np.array,
-                    service_frequencies=np.array([2, 3, 2, 2], dtype=np.int8),
+                    required_services=np.array([2, 3, 2, 2], dtype=np.int8),
                     ) -> np.ndarray:
     """Generate visit days for each installation by randomly assign an
     installation to each installation i.
@@ -12,12 +12,12 @@ def generate_visits(n_installations: int,
     With three installations and 7 days visits array can look like this:
 
         array([[False, False, False,  True, False, False, False],
-           [False,  True,  True, False, False, False,  True],
-           [ True, False, False,  True,  True,  True,  True]])
+               [False,  True,  True, False, False, False,  True],
+               [ True, False, False,  True,  True,  True,  True]])
 
 
     Args:
-        service_frequencies (np.array): Array of length n_insteallations with
+        required_services (np.array): Array of length n_insteallations with
             required service frequencies for each installation.
         n_installations (int): Number of installations
         n_days (int): Number of days in period
@@ -28,20 +28,65 @@ def generate_visits(n_installations: int,
         np.ndarray: Boolean array of size (n_installations, n_days)
                             with visit days set to True.
     """
-    rng = np.random.default_rng()
 
     # Initialize visits
-    visits = np.zeros((n_installations, n_days), dtype=bool)
+    visits = np.full((n_installations, n_days),
+                     fill_value=False,
+                     dtype=bool)
 
     # Randomly fill visits for each service frequency requirement
-    for i in range(n_installations):
-        visit_days = rng.choice(days,
-                                size=service_frequencies[i],
-                                replace=False
-                                )
-        visits[i, visit_days] = True
+    for inst in range(n_installations):
+
+        # Spread installations by selecting a distance between min and
+        # max spread (retry if still not sufficiently spread)
+        Pf_max = n_days // required_services[inst]
+        # If division is even, space between services is equal
+        if Pf_max == n_days / required_services[inst]:
+            Pf_min = Pf_max = Pf_max - 1
+        else:
+            Pf_min = Pf_max - 1
+
+        def generate_service_days():
+            # Which days should installation i be serviced
+            service_days = np.full(shape=required_services[inst],
+                                   fill_value=0,
+                                   dtype=np.int8)
+
+            while True:
+                # Place first day between 0 and pfmax
+                service_days[0] = random.randint(0, Pf_max)
+
+                # If more days, space between is between pf min and pfmax
+                if required_services[inst] >= 2:
+                    for j in range(1, required_services[inst]):
+                        service_days[j] = service_days[j-1] + random.randint(Pf_min,
+                                                                             Pf_max) + 1
+                # Ensure space between last and first service between Pf_min and Pf_max
+                if (Pf_min <= (service_days[0] -
+                               (service_days[-1] - (n_days - 1))) <= Pf_max):
+                    break
+                else:
+                    continue  # If not, try again
+
+            return service_days
+
+        while True:
+            service_days = generate_service_days()
+
+            # service days may generate days that are too far spread out
+            try:
+                # Set chosen service days in boolean visits array
+                visits[inst, service_days] = True
+                break  # Successfully found properly spread services
+            except IndexError:
+                pass  # Keep looking for proper service days
 
     return visits
+
+
+def generate_departures_from_visits(visits: np.ndarray,
+                                    ) -> np.ndarray:
+    pass
 
 
 def generate_visits_from_routes(routes: np.ndarray,
--- todo.md
+++ todo.md
@@ -4,3 +4,9 @@ TODO:
     2. Generate departures p. 133
 - Penalized cost page 131
 
+Tests:
+- Ensure generate_visits() passes constraints check.
+
+Checks:
+- No more services required than days in period
+
